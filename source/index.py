392e91657fbd031eef44cc48d73340f4e34124e6
JPelda <johannes.pelda@hawk-hhg.de>
2018-06-21 19:45:05 +0200

    first sphinx

---
+++ source/index.py
@@ -0,0 +1,20 @@
+.. MEMPHIS documentation master file, created by
+   sphinx-quickstart on Thu Jun 21 19:44:48 2018.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to MEMPHIS's documentation!
+===================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
